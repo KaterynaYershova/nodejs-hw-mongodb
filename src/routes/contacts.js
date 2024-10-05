75187d1e480513ea1fb6656a56a782ff3d8d64a1
Kateryna Yershova <157507778+KaterynaYershova@users.noreply.github.com>
2024-10-05 18:24:30 +0300

    add changes

---
+++ src/routes/contacts.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  try {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get('/:contactId', async (req, res) => {
+  const { contactId } = req.params;
+  try {
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default router;
