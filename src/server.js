1a75c552731a182c7a29ba8c5e5ab41d613b674b
Kateryna_Yershova <yershova.kate@icloud.com>
2024-10-13 12:34:55 +0300

    add changes

--- src/server.js
+++ src/server.js
@@ -1,22 +1,21 @@
 import express from 'express';
+import logger from 'morgan';
 import cors from 'cors';
-import pino from 'pino-http';
 import contactsRouter from './routes/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
-export const setupServer = () => {
-  const app = express();
+const app = express();
 
-  app.use(cors());
-  app.use(pino());
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
 
-  app.use('/contacts', contactsRouter);
+app.use('/contacts', contactsRouter);
 
-  app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
-  });
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-};
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
