10ff1ed1a6ef3e63d9f2a2e252049881d5dd1375
Kateryna Yershova <157507778+KaterynaYershova@users.noreply.github.com>
2024-10-13 12:54:22 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,17 +5,19 @@ import contactsRouter from './routes/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
-const app = express();
+export const setupServer = () => {
+  const app = express();
 
-app.use(logger('dev'));
-app.use(cors());
-app.use(express.json());
+  app.use(logger('dev'));
+  app.use(cors());
+  app.use(express.json());
 
-app.use('/contacts', contactsRouter);
+  app.use('/contacts', contactsRouter);
 
-app.use(notFoundHandler);
-app.use(errorHandler);
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-});
+  app.listen(3000, () => {
+    console.log('Server running on port 3000');
+  });
+};
