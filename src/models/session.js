b935b802b69e39e37b8161cb6815b62432d74520
Kateryna_Yershova <yershova.kate@icloud.com>
2024-10-28 19:30:41 +0200

    add changes

--- src/models/session.js
+++ src/models/session.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const sessionSchema = new mongoose.Schema({
     userId: { 
@@ -26,4 +26,5 @@ const sessionSchema = new mongoose.Schema({
     timestamps: true  
 });
 
-module.exports = mongoose.model('Session', sessionSchema);
+const Session = mongoose.model('Session', sessionSchema);
+export default Session;
