9e247d49a7c0087570382cf99977603bfacbd03d
Kateryna_Yershova <yershova.kate@icloud.com>
2024-11-03 14:03:18 +0200

    add changes

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.use(authenticate);
 
-router.get('/', ctrlWrapper(getContactsController));
+router.get('/', ctrlWrapper(ctrl.getContacts));
 
 router.get('/:contactId', isValidId, ctrlWrapper(ctrl.getContactById));
 
