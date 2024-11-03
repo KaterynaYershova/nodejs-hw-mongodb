21c3dd2fa0f859ffa19a74f8e6338aba1e477f20
Kateryna_Yershova <yershova.kate@icloud.com>
2024-11-03 15:34:43 +0200

    add changes

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,14 +1,12 @@
-import bcrypt from 'bcrypt';
-import createHttpError from 'create-http-error';
-import User from '../models/user.js';
-import Session from '../models/session.js';
-import { logoutUser, refreshUsersSession } from '../services/auth.js';
-import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'; 
+import createHttpError from 'create-http-error'; 
+import User from '../models/user.js'; 
+import Session from '../models/session.js'; 
+import { logoutUser, refreshUsersSession } from '../services/auth.js'; 
+import { randomBytes } from 'crypto'; 
 
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || 'your_access_token_secret';
-const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || 'your_refresh_token_secret';
-const ACCESS_TOKEN_EXPIRATION = '15m';
-const REFRESH_TOKEN_EXPIRATION = '30d';
+const FIFTEEN_MINUTES = 15 * 60 * 1000; 
+const ONE_DAY = 30 * 24 * 60 * 60 * 1000; 
 
 export const register = async (req, res, next) => {
     const { name, email, password } = req.body;
@@ -45,19 +43,20 @@ export const login = async (req, res, next) => {
             throw createHttpError(401, 'Invalid email or password');
         }
 
-        const accessToken = jwt.sign({ userId: user._id }, ACCESS_TOKEN_SECRET, { expiresIn: ACCESS_TOKEN_EXPIRATION });
-        const refreshToken = jwt.sign({ userId: user._id }, REFRESH_TOKEN_SECRET, { expiresIn: REFRESH_TOKEN_EXPIRATION });
+        const accessToken = randomBytes(30).toString('base64'); 
+        const refreshToken = randomBytes(30).toString('base64'); 
 
         await Session.findOneAndDelete({ userId: user._id });
         await Session.create({
             userId: user._id,
             accessToken,
             refreshToken,
-            accessTokenValidUntil: Date.now() + 15 * 60 * 1000,
-            refreshTokenValidUntil: Date.now() + 30 * 24 * 60 * 60 * 1000
+            accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES), 
+            refreshTokenValidUntil: new Date(Date.now() + ONE_DAY) 
         });
 
         res.cookie('refreshToken', refreshToken, { httpOnly: true });
+        res.cookie('sessionId', refreshToken, { httpOnly: true, expires: new Date(Date.now() + ONE_DAY) }); 
         res.status(200).json({
             status: 200,
             message: 'Successfully logged in a user!',
@@ -70,41 +69,43 @@ export const login = async (req, res, next) => {
     }
 };
 
-const setupSession = (res, session) => {
-    res.cookie('refreshToken', session.refreshToken, {
-      httpOnly: true,
-      expires: new Date(Date.now() + ONE_DAY),
-    });
-    res.cookie('sessionId', session._id, {
-      httpOnly: true,
-      expires: new Date(Date.now() + ONE_DAY),
-    });
-  };
-  
-  export const refreshUserSessionController = async (req, res) => {
+export const refreshUserSessionController = async (req, res) => {
+    if (!req.cookies.sessionId) {
+        throw createHttpError(400, 'Session ID is missing');
+    }
+
     const session = await refreshUsersSession({
-      sessionId: req.cookies.sessionId,
-      refreshToken: req.cookies.refreshToken,
+        sessionId: req.cookies.sessionId,
+        refreshToken: req.cookies.refreshToken,
     });
-  
-    setupSession(res, session);
-  
+
+    res.cookie('refreshToken', session.refreshToken, {
+        httpOnly: true,
+        expires: new Date(Date.now() + ONE_DAY),
+    });
+    res.cookie('sessionId', sessionId, {
+        httpOnly: true,
+        expires: new Date(Date.now() + ONE_DAY),
+    });
+
     res.json({
-      status: 200,
-      message: 'Successfully refreshed a session!',
-      data: {
-        accessToken: session.accessToken,
-      },
+        status: 200,
+        message: 'Successfully refreshed a session!',
+        data: {
+            accessToken: session.accessToken,
+        },
     });
-  };
+};
 
 export const logoutUserController = async (req, res) => {
-    if (req.cookies.sessionId) {
-      await logoutUser(req.cookies.sessionId);
+    if (!req.cookies.sessionId) {
+        throw createHttpError(400, 'Session ID is missing');
     }
-  
+
+    await logoutUser(req.cookies.sessionId);
+
     res.clearCookie('sessionId');
     res.clearCookie('refreshToken');
-  
+
     res.status(204).send();
-  };
+};
