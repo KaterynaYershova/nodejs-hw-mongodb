cd096b78ce57758044df9489d2f5b664056372a6
Kateryna_Yershova <yershova.kate@icloud.com>
2024-11-12 18:23:58 +0200

    add changes

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ import authRouter from './routes/auth.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
-import { swaggerDocs } from './middlewares/swaggerDocs.js'; 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../docs/swagger.json'; 
 
 export const setupServer = () => {
   const app = express();
@@ -19,7 +20,7 @@ export const setupServer = () => {
 
   app.use('/uploads', express.static(UPLOAD_DIR));
 
-  app.use('/api-docs', swaggerDocs());
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
